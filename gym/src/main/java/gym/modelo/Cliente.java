5116cd5fe543269381b58d5a0d792cc0ab22df31
Brenda <brentorres26@gmail.com>
2023-09-26 17:19:38 -0300

    Creacion clase Cliente

---
+++ gym/src/main/java/gym/modelo/Cliente.java
@@ -0,0 +1,71 @@
+package gym.modelo;
+
+public class Cliente {
+
+	Integer id;
+	String nombre;
+	String apellido;
+	String direccion;
+	String telefono;
+	boolean pago = false;
+	
+	public Cliente(Integer id, String nombre, String apellido, String direccion, String telefono, boolean pago) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.pago = pago;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public boolean isPago() {
+		return pago;
+	}
+
+	public void setPago(boolean pago) {
+		this.pago = pago;
+	}
+	
+	
+}
