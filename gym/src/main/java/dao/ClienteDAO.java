853d59cbba2cd57785f4cf57b30d9becddffef57
Brenda <brentorres26@gmail.com>
2023-10-01 10:28:20 -0300

    Script eliminar y modificar

--- gym/src/main/java/dao/ClienteDAO.java
+++ gym/src/main/java/dao/ClienteDAO.java
@@ -34,5 +34,55 @@ public class ClienteDAO {
     						"fue insertado el producto %s ",
     						cliente));
     				
-    	}}
+    			}
+    		}
+    
+    
+    //guardar
+    
+    public void guardar(Cliente cliente) {
+		try {
+			String sql = "INSERT INTO clientes (nombre, apellido, direccion) VALUES (?, ?, ?)";
+
+			try (PreparedStatement pstm = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+				pstm.setString(1, cliente.getNombre());
+				pstm.setString(2, cliente.getApellido());
+				pstm.setString(3, cliente.getDireccion());
+
+				pstm.execute();
+
+				try (ResultSet rst = pstm.getGeneratedKeys()) {
+					while (rst.next()) {
+						cliente.setId(rst.getInt(1));
+					}
+				}
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+    
+    //eliminar
+    public void Eliminar(Integer id) {
+		try (PreparedStatement stm = con.prepareStatement("DELETE FROM clientes WHERE id = ?")) {
+			stm.setInt(1, id);
+			stm.execute();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+    
+    //EDITAR
+    public void Actualizar(String nombre, String apellido, String domicilio) {
+		try (PreparedStatement stm = con
+				.prepareStatement("UPDATE clientes SET nombre = ?, apellido = ?, domicilio = ? WHERE id = ?")) {
+			stm.setString(1, nombre);
+			stm.setString(2, apellido);
+			stm.setString(3, domicilio);
+			stm.execute();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
