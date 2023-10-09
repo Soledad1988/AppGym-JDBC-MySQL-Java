6c368e1a7402d1e053a66bb499aea37a487a50cf
Brenda <brentorres26@gmail.com>
2023-10-09 14:56:58 -0300

    Creacion vista Menú

---
+++ gym/src/main/java/vista/MenuFrame.java
@@ -0,0 +1,109 @@
+package vista;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
+public class MenuFrame extends JFrame {
+
+	private JPanel contentPane;
+	 private JButton botonNuevoClientes;
+	 
+	 ListaClientesFrame listaClientesFrame;
+	 ReporteClientes reporteClientes;
+	
+	/*public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuFrame frame = new MenuFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	
+	public MenuFrame() {
+		super("Gym-Fittnes");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 647, 375);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel titulo = new JLabel("Gym - FITTNES");
+		titulo.setFont(new Font("Tw Cen MT", Font.BOLD | Font.ITALIC, 26));
+		titulo.setBounds(245, 11, 180, 40);
+		contentPane.add(titulo);
+		
+		JButton botonNuevoClientes = new JButton("Nuevo Cliente");
+		botonNuevoClientes.setBounds(91, 102, 115, 23);
+		contentPane.add(botonNuevoClientes);
+		
+		botonNuevoClientes.addActionListener(new ActionListener() {
+        	
+            public void actionPerformed(ActionEvent e) { 
+            	try {
+					abrirNuevoCliente();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+            }
+            
+        });
+		
+		JButton listaClientes = new JButton("Lista Clientes");
+		listaClientes.setBounds(91, 149, 115, 23);
+		contentPane.add(listaClientes);
+		
+		listaClientes.addActionListener(new ActionListener() {
+        	
+            public void actionPerformed(ActionEvent e) { 
+            	try {
+            		abrirReporte();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+            }
+            
+        });
+		
+		JButton reporteGastos = new JButton("Reporte Gastos");
+		reporteGastos.setBounds(91, 200, 115, 23);
+		contentPane.add(reporteGastos);
+		
+		JLabel logo = new JLabel("New label");
+		logo.setIcon(new ImageIcon("D:\\Users\\Brenda\\Downloads\\gym.png"));
+		logo.setBounds(326, 34, 274, 243);
+		contentPane.add(logo);
+
+		setLocationRelativeTo(null);
+		
+       
+	}
+	
+	   private void abrirNuevoCliente() throws SQLException {
+	       listaClientesFrame = new ListaClientesFrame();
+	    }
+	   
+	   private void abrirReporte() throws SQLException {
+	       reporteClientes = new ReporteClientes();
+	    }
+}
