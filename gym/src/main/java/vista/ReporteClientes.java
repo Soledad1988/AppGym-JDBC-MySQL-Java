a57a3ea536e1abe756c5a83dbd819c8328fac467
Brenda <brentorres26@gmail.com>
2023-10-09 14:56:37 -0300

    Modificacion vista Reporte Clientes

--- gym/src/main/java/vista/ReporteClientes.java
+++ gym/src/main/java/vista/ReporteClientes.java
@@ -1,13 +1,32 @@
 package vista;
 
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 
+import com.toedter.calendar.JDateChooser;
+
 import controller.ClienteController;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 
 public class ReporteClientes extends JFrame {
@@ -15,39 +34,115 @@ public class ReporteClientes extends JFrame {
     private static final long serialVersionUID = 1L;
 
     private JTable tablaReporte;
+    private JTable tabla;
     private DefaultTableModel modelo;
+    
+    //********************************
+    private JPanel contentPane;
+    private JLabel labelPeriodo;
+	
+
+	private JButton botonGuardar, botonModificar, botonLimpiar, botonEliminar, botonReporte;
+	
 
     private ClienteController clienteController;
+    private JTextField textTotal;
+    
+    
+    public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ReporteClientes frame = new ReporteClientes();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-    public ReporteClientes(ListaClientesFrame listaClientesFrame) throws SQLException {
-        super("Reporte");
-
-        this.clienteController = new ClienteController();
-
-        Container container = getContentPane();
-        setLayout(null);
-
-        tablaReporte = new JTable();
-        tablaReporte.setBounds(0, 0, 600, 400);
-        container.add(tablaReporte);
-
-        modelo = (DefaultTableModel) tablaReporte.getModel();
-        modelo.addColumn("");
-        modelo.addColumn("");
-        modelo.addColumn("");
-
-        cargaReporte();
-
-        setSize(600, 400);
-        setVisible(true);
-        setLocationRelativeTo(listaClientesFrame);
-    }
-
-    private void cargaReporte() throws SQLException {
-        var contenido = clienteController.cargaReporte();
+    public ReporteClientes() {
+    	super("Lista Clientes");
+    	this.clienteController = new ClienteController();
+    	
+    	Container container = getContentPane();
+        getContentPane().setLayout(null);
         
-        contenido.forEach(fila -> modelo
-                .addRow(new Object[] {}));
+    	configurarTablaDeContenido(container);
+    	
+    	JLabel lblNewLabel = new JLabel("Reporte de Clientes");
+    	lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 18));
+    	lblNewLabel.setBounds(309, 21, 218, 36);
+    	getContentPane().add(lblNewLabel);
+    	
+    	JLabel lblPeriodo = new JLabel("Periodo");
+    	lblPeriodo.setFont(new Font("Tahoma", Font.PLAIN, 15));
+    	lblPeriodo.setBounds(170, 108, 100, 22);
+    	getContentPane().add(lblPeriodo);
+    	
+    	JComboBox BoxPeriodo = new JComboBox();
+    	BoxPeriodo.setModel(new DefaultComboBoxModel(new String[] {"ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"}));
+    	BoxPeriodo.setBounds(429, 105, 148, 22);
+    	getContentPane().add(BoxPeriodo);
+    	
+    	JLabel total = new JLabel("Total");
+    	total.setFont(new Font("Tahoma", Font.PLAIN, 15));
+    	total.setBounds(339, 494, 65, 20);
+    	getContentPane().add(total);
+    	
+    	textTotal = new JTextField();
+    	textTotal.setEditable(false);
+    	textTotal.setBounds(455, 497, 86, 20);
+    	getContentPane().add(textTotal);
+    	textTotal.setColumns(10);
+    	
+    }
+    
+    
+    private void configurarTablaDeContenido(Container container) {
+        tabla = new JTable();
+
+        modelo = (DefaultTableModel) tabla.getModel();
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Apellido");
+    
+        cargarTabla();
+
+        tabla.setBounds(10, 205, 760, 280);
+
+        botonEliminar = new JButton("Deudores");
+        botonModificar = new JButton("Modificar");
+        botonReporte = new JButton("Ver Reporte");
+        botonEliminar.setBounds(10, 500, 80, 20);
+        botonModificar.setBounds(100, 500, 80, 20);
+        botonReporte.setBounds(190, 500, 80, 20);
+
+        container.add(tabla);
+        /*container.add(botonEliminar);
+        container.add(botonModificar);
+        container.add(botonReporte);*/
+
+        setSize(800, 600);
+        setVisible(true);
+        setLocationRelativeTo(null);
     }
 
+
+
+    private void cargarTabla() {
+        List<Map<String, String>> clientes = new ArrayList<Map<String, String>>();
+
+        try {
+        	clientes = this.clienteController.reporte();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        clientes.forEach(producto -> modelo.addRow(
+                new Object[] {
+                        producto.get("nombre"),
+                        producto.get("apellido")}));
+    }
 }
