84a2088355653247048c716798b3caf827c48c5c
Brenda <brentorres26@gmail.com>
2023-10-09 14:57:31 -0300

    Modificacion mein

--- gym/src/main/java/controller/GymMein.java
+++ gym/src/main/java/controller/GymMein.java
@@ -1,16 +1,31 @@
 package controller;
 
+import java.awt.EventQueue;
 import java.sql.SQLException;
 
 import javax.swing.JFrame;
 
 import vista.ListaClientesFrame;
+import vista.MenuFrame;
 
 public class GymMein {
 
 	public static void main(String[] args) throws SQLException {
 		
-		ListaClientesFrame listaClientes = new ListaClientesFrame();
-		listaClientes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/*ListaClientesFrame listaClientes = new ListaClientesFrame();
+		listaClientes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuFrame frame = new MenuFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	
+		
 	}
 }
