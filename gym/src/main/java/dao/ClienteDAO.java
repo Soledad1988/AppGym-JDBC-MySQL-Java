ea0ba33ce22c91259ebc7a413cd7fdd2e6a11dcf
Brenda <brentorres26@gmail.com>
2023-10-12 14:30:27 -0300

    Se modificaron paramatros de la clase DAO Clientes

--- gym/src/main/java/dao/ClienteDAO.java
+++ gym/src/main/java/dao/ClienteDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,11 +22,13 @@ public class ClienteDAO {
     //guardar
     public void guardar(Cliente cliente) {
 		try {
-			String sql = "INSERT INTO clientes (nombre, apellido, direccion) VALUES (?, ?, ?)";
+			String sql = "INSERT INTO clientes (fechaAlta, nombre, apellido, direccion, precio) VALUES (?, ?, ?, ?, ?)";
 			try (PreparedStatement pstm = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-				pstm.setString(1, cliente.getNombre());
-				pstm.setString(2, cliente.getApellido());
-				pstm.setString(3, cliente.getDireccion());
+				pstm.setDate(1, cliente.getFechaAlta());
+				pstm.setString(2, cliente.getNombre());
+				pstm.setString(3, cliente.getApellido());
+				pstm.setString(4, cliente.getDireccion());
+				pstm.setDouble(5, cliente.getPrecio());
 				pstm.execute();
 				try (ResultSet rst = pstm.getGeneratedKeys()) {
 					while (rst.next()) {
@@ -50,13 +53,15 @@ public class ClienteDAO {
 	}
     
     //EDITAR
-    public void Actualizar(String nombre, String apellido, String direccion, Integer id) {
-    	String sql = "UPDATE clientes SET nombre = ?, apellido = ?, direccion = ? WHERE id = ?";
+    public void Actualizar(Date fechaAlta, String nombre, String apellido, String direccion, Double precio, Integer id) {
+    	String sql = "UPDATE clientes SET fechaAlta = ? nombre = ?, apellido = ?, direccion = ? precio = ? WHERE id = ?";
 		try (PreparedStatement stm = con.prepareStatement(sql)) {
-			stm.setString(1, nombre);
-			stm.setString(2, apellido);
-			stm.setString(3, direccion);
-			stm.setInt(4, id);
+			stm.setDate(1, fechaAlta);
+			stm.setString(2, nombre);
+			stm.setString(3, apellido);
+			stm.setString(4, direccion);
+			stm.setDouble(4, precio);
+			stm.setInt(5, id);
 			stm.execute();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
@@ -66,7 +71,8 @@ public class ClienteDAO {
     private void transformarResultSetEnCliente(List<Cliente> clientes, PreparedStatement pstm) throws SQLException {
 		try (ResultSet rst = pstm.getResultSet()) {
 			while (rst.next()) {
-				Cliente cli = new Cliente(rst.getInt(1), rst.getString(2), rst.getString(3), rst.getString(4));
+				Cliente cli = new Cliente(rst.getInt(1), rst.getDate(2), rst.getString(3), rst.getString(4),
+						rst.getString(5), rst.getDouble(6));
 				clientes.add((Cliente) clientes);
 			}
 		}				
