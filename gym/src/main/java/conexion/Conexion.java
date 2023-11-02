dc6129b55d16a7d32d1ccbe8c6f4889366eb3b34
brent <brent@192.168.0.13>
2023-11-02 15:48:09 -0300

    Pruebas de pool de conexiones

--- gym/src/main/java/conexion/Conexion.java
+++ gym/src/main/java/conexion/Conexion.java
@@ -6,17 +6,40 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 
 //conectamos a la base de datos
 public class Conexion {
+	
+	public DataSource dataSource;
 
-	public Connection recuperaConexion() throws SQLException {
+	public Conexion() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/baseClientes?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("root");
+		
+
+		this.dataSource = comboPooledDataSource;
+	}
+
+	public Connection recuperarConexion() {
+		try {
+			return this.dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/*public Connection recuperaConexion() throws SQLException {
 	
 		return DriverManager.getConnection(
               "jdbc:mysql://localhost/baseClientes?useTimeZone=true&serverTimeZone=UTC",
               "root",
               "root");
-	}
+	}*/
+
 	
 }
 
