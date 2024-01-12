08f3e8e8128889724129cd04484780ddc2c392c8
Brenda <brentorres26@gmail.com>
2024-01-12 10:31:59 -0300

    Cambios en reportes

--- gym/src/main/java/dao/ReporteIngresosDAO.java
+++ gym/src/main/java/dao/ReporteIngresosDAO.java
@@ -20,50 +20,50 @@ public class ReporteIngresosDAO {
 	}
 
 	//listar reporte
-			public List<Map<String,String>> reporteClientes() throws SQLException {
-				Conexion factory = new Conexion();
-				final Connection con = factory.recuperaConexion();
+	public List<Map<String,String>> reporteClientes() throws SQLException {
+		Conexion factory = new Conexion();
+		final Connection con = factory.recuperaConexion();
 				  
-					final PreparedStatement statement = con.prepareStatement("SELECT fechaAlta, nombre, apellido, telefono FROM clientes");
+		final PreparedStatement statement = con.prepareStatement("SELECT fechaAlta, nombre, apellido, telefono FROM clientes");
 			        
-					statement.execute();
+		statement.execute();
 
-			        ResultSet resultSet = statement.getResultSet();
+		ResultSet resultSet = statement.getResultSet();
 
-			        List<Map<String, String>> resultado = new ArrayList<>();
+		List<Map<String, String>> resultado = new ArrayList<>();
 
 			      //leemos el contendio para agregrlo a un listado
-			        while (resultSet.next()) {
-			            Map<String, String> fila = new HashMap<>();
-			            fila.put("fechaAlta", String.valueOf(resultSet.getDate("fechaAlta")));
-			            fila.put("nombre", resultSet.getString("nombre"));
-			            fila.put("apellido", resultSet.getString("apellido"));
-			            fila.put("telefono", resultSet.getString("telefono"));
+		while (resultSet.next()) {
+			 Map<String, String> fila = new HashMap<>();
+			 fila.put("fechaAlta", String.valueOf(resultSet.getDate("fechaAlta")));
+			 fila.put("nombre", resultSet.getString("nombre"));
+			 fila.put("apellido", resultSet.getString("apellido"));
+			 fila.put("telefono", resultSet.getString("telefono"));
 
-			            resultado.add(fila);
-			        }
+			 resultado.add(fila);
+		  }
 					
-				return resultado;
+		return resultado;
 			
-			}
+	}
 			
 		    
-		    public static double realizarSumaPorMes(int numeroMes) throws SQLException {
-		        Conexion factory = new Conexion();
-		        final Connection con = factory.recuperaConexion();
-		        ResultSet resultSet = null;
+	public static double realizarSumaPorMes(int numeroMes) throws SQLException {
+		    Conexion factory = new Conexion();
+		    final Connection con = factory.recuperaConexion();
+		    ResultSet resultSet = null;
 
-		        try {
-		            String consulta = "SELECT SUM(monto) AS resultado FROM cuotas WHERE MONTH(fechaPago) = ?";
-		            PreparedStatement statement = con.prepareStatement(consulta);
+		    try {
+		        String consulta = "SELECT SUM(monto) AS resultado FROM cuotas WHERE MONTH(fechaPago) = ?";
+		        PreparedStatement statement = con.prepareStatement(consulta);
 		            
-		            statement.setInt(1, numeroMes);
+		        statement.setInt(1, numeroMes);
 
-		            // Ejecutar la consulta
-		            resultSet = statement.executeQuery();
+		        // Ejecutar la consulta
+		        resultSet = statement.executeQuery();
 
-		            // Procesar el resultado
-		            if (resultSet.next()) {
+		        // Procesar el resultado
+		        if (resultSet.next()) {
 		                return resultSet.getDouble("resultado");
 		            }
 		        } catch (SQLException e) {
