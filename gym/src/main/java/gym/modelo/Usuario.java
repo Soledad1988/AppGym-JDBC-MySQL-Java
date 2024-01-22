7586cedbdbf451c20924b94692162bba179c8ea0
Brenda <brentorres26@gmail.com>
2024-01-22 17:59:05 -0300

    Se agrego campo habilitado en modelo Usuario

--- gym/src/main/java/gym/modelo/Usuario.java
+++ gym/src/main/java/gym/modelo/Usuario.java
@@ -9,24 +9,33 @@ public class Usuario {
 	private Integer idUsuario; 
 	private String nombreUsuario;
 	private String contrasena;
+	private boolean habilitado;
 	private Set<Rol> roles = new HashSet<>();
 	
 	public Usuario() {
 		
 	}
 
-	public Usuario(Integer idUsuario, String nombreUsuario, String contrasena) {
+	public Usuario(Integer idUsuario, String nombreUsuario, String contrasena, boolean habilitado, Set<Rol> roles) {
 		super();
 		this.idUsuario = idUsuario;
 		this.nombreUsuario = nombreUsuario;
 		this.contrasena = contrasena;
+		this.habilitado = habilitado;
+		this.roles = roles;
 	}
 
-	
-	public Usuario(String nombreUsuario, String contrasena) {
-		super();
+	public Usuario(Integer idUsuario, String nombreUsuario, String contrasena, boolean habilitado) {
+		this.idUsuario = idUsuario;
 		this.nombreUsuario = nombreUsuario;
 		this.contrasena = contrasena;
+		this.habilitado = habilitado;
+	}
+
+	public Usuario(String nombreUsuario, String contrasena, boolean habilitado) {
+		this.nombreUsuario = nombreUsuario;
+		this.contrasena = contrasena;
+		this.habilitado = habilitado;
 	}
 
 	public Integer getIdUsuario() {
@@ -52,27 +61,34 @@ public class Usuario {
 	public void setContrasena(String contrasena) {
 		this.contrasena = contrasena;
 	}
-	
 
-    public void setRoles(Set<Rol> roles) {
-        this.roles = roles;
-    }
-    
-    public void addRol(Rol rol) {
+	public boolean isHabilitado() {
+		return habilitado;
+	}
+
+	public void setHabilitado(boolean habilitado) {
+		this.habilitado = habilitado;
+	}
+
+	public Set<Rol> getRoles() {
+		return roles;
+	
+	}
+	
+	public void addRol(Rol rol) {
         roles.add(rol);
     }
 
-    public Set<Rol> getRoles() {
-        return roles;
-    }
-    
-    //Convertir los roles a string antes, asi cargar la tabla en la vista
+	public void setRoles(Set<Rol> roles) {
+		this.roles = roles;
+	}
+	
+	 //Convertir los roles a string antes, asi cargar la tabla en la vista
     public String getRolesAsString() {
  	    return roles.stream()
  	                .map(Rol::toString)
  	                .collect(Collectors.joining(", "));
  	}
-
 	
 	
 }
